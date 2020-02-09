6ef5ad0a5efd1808c578ddb6a095579cf2d3d0ff
tetunori <tetunori@gmail.com>
2020-02-09 17:02:09 +0900

    1st Commit(js)

---
+++ index.js
@@ -0,0 +1,12 @@
+
+var stringForToioJs = '\n' + 
+    '/* Sound data converted from MIDI file */\n' + 
+    '\n' + 'const melody = [\n';
+			stringForToioJs += '];' + '\n\n\n\n\n\n\n\n';
+			document.getElementById("output").innerHTML = stringForToioJs;
+document.getElementById("output").innerHTML = stringForToioJs;
+document.getElementById("output").setAttribute("class", "prettyprint lang-js linenums");
+PR.prettyPrint();
+
+// execute dropify.
+$('.dropify').dropify();
